bf8bc92280e3ffcf5679f7be0aaf6f9b28278af8
Matteo Kinkela <mkinkela123@gmail.com>
2020-12-08 21:30:18 +0100

    Updating a Task's status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import {Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
-import {Task} from "./tasks.model";
+import {Task, TaskStatus} from "./tasks.model";
 import {CreateTaskDto} from "./dto/CreateTask.dto";
 
 @Controller('tasks')
@@ -25,5 +25,13 @@ export class TasksController {
     @Delete('/:id')
     removeTaskById(@Param('id') id: string): void {
         return this.tasksService.removeTaskById(id);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id') id: string,
+        @Body('status') status: TaskStatus
+    ): Task {
+        return this.tasksService.updateTaskStatus(id, status)
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -33,4 +33,12 @@ export class TasksService {
     removeTaskById(id: string): void {
         this.tasks = this.tasks.filter(task => task.id !== id);
     }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+
+        const task: Task = this.getTaskById(id);
+        task.status = status;
+
+        return task;
+    }
 }
